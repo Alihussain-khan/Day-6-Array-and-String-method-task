f691173905734f72cdf014eafdaa3e28a508b233
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-01-24 13:02:11 +0500

    map task complete

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
 console.log("running");
 
 //Task a
-const set1 = new Set(["a", "b", "c"]);
-const set2 = new Set(["d", "e", "f"]);
+const originalArray = [1, 2, 3, 4, 5];
 
-//Task b
-const set3 = new Set([...set1, ...set2]);
-console.log(set3);
-
-//Task c
-const subset = Array.from(set1);
-const set = Array.from(set3);
-
-let checkSubset = (set, subset) => {
-  return subset.every((e) => {
-    return set.includes(e);
-  });
-};
-console.log(checkSubset(set, subset));
-
-//Task d
-const reduced = set2.delete("d");
-console.log(set2);
-
-//Task e
-const subset1 = Array.from(set1);
-console.log(subset1);
+//Tasl b
+let a = originalArray.map((e) => {
+  return e * e;
+});
+console.log(a);
