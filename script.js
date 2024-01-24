ef884b3d8707e03c99630aad6a345d8b45ce9dbf
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-01-24 12:18:43 +0500

    Task 2 done

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ let message = "Hello, World!";
 console.log(message.substring(0, 5));
 
 //Task c
+console.log(message.toLocaleUpperCase());
+
+//Task d
+let includes = message.match(/world/i);
+if (includes == null) {
+  console.log("flase");
+} else {
+  console.log("True");
+}
+
+//Task e
+let replaced = message.replace("World", "Ali");
+console.log(replaced);
+
+//Task f
+//easier way
+const arr = [...message].map((e) => e);
+// console.log(message.split(""));
+console.log(arr);
