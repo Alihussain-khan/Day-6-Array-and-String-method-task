2fc2b1a7966ca933f0e96c81bd4c1972e0c61ea2
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-01-24 11:31:16 +0500

    Branch 2 setup

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
 console.log("running");
 
 //Task a
-const numbers = [1, 2, 3, 4, 5];
+let message = "Hello, World!";
 
 //Task b
-numbers.push(6);
-//Another way of achieving task b
-// numbers[numbers.length] = 6;
-console.log("Array after adding 6th element " + numbers);
+console.log(message.substring(0, 5));
 
 //Task c
-numbers.shift();
-console.log("Array After removing first element " + numbers);
-
-//Task d
-console.log("Array after reverse " + numbers.reverse());
-
-//Task e
-console.log("Array after joinging with a comma " + numbers.join(","));
-
-//Task f
-const numbers2 = numbers.map(squared);
-
-function squared(value, index, array) {
-  return value * value;
-}
-console.log("Squared Array " + numbers2);
