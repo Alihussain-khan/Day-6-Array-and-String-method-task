7c7195bd8a7f5d80f49cd2f3b9f5c2483e47305d
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-01-24 12:59:12 +0500

    set task complete

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 console.log("running");
 
 //Task a
-let a = 25;
-console.log(Math.sqrt(a));
+const set1 = new Set(["a", "b", "c"]);
+const set2 = new Set(["d", "e", "f"]);
 
 //Task b
-console.log(Math.floor(Math.random() * 11)) + 1;
+const set3 = new Set([...set1, ...set2]);
+console.log(set3);
 
 //Task c
-console.log(Math.round(Math.PI));
+const subset = Array.from(set1);
+const set = Array.from(set3);
+
+let checkSubset = (set, subset) => {
+  return subset.every((e) => {
+    return set.includes(e);
+  });
+};
+console.log(checkSubset(set, subset));
+
+//Task d
+const reduced = set2.delete("d");
+console.log(set2);
+
+//Task e
+const subset1 = Array.from(set1);
+console.log(subset1);
